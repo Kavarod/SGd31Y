662d926861231b72cd4ee7bced7650edf2f185db
Kaloyan Blagoev <kaloyan.iv.blgaoev@gmail.com>
2022-11-23 14:35:27 +0100

    working multiplexer

--- client.js
+++ client.js
@@ -1,11 +1,19 @@
 const fs = require("fs");
 const io = require("socket.io-client");
-const socket = io("http://localhost:3000");
 const request = require("request");
+//Local camera jpeg consumer library.
 const MjpegConsumer = require("./MjpegConsumer");
 
-//! Key State.
-let camerarequest = null;
+//* connect to socket with Auth Token.
+const socket = io("https://nameless-hollows-47413.herokuapp.com", {
+	transportOptions: {
+		polling: {
+			extraHeaders: {
+				Authorization: "Bearer Infinno#Bathomatic123",
+			},
+		},
+	},
+});
 
 socket.on("connect", () => {
 	console.log("Client connected.");
@@ -13,49 +21,65 @@ socket.on("connect", () => {
 
 socket.on("disconnect", () => {
 	console.log("Client disconnected.");
+	//Try to reconnect.
 	socket.connect();
 });
 
-socket.on("validId", (id) => {
-	
-	console.log("Client has been given a valid id!");
-	const consumer = new MjpegConsumer();
+//! Key App state.
+//Holds all camera connections + 1 buffer.
+let camerarequests = [null, null, null, null, null, null];
+//* Read JSON data once!
+const CamData = JSON.parse(
+	fs.readFileSync("./cameraStorage.json", "utf-8")
+).cams;
 
-	const CamData = JSON.parse(
-		fs.readFileSync("./CameraStorage.json", "utf-8")
-	).cams;
+socket.on("validId", (id) => {
+
+	// holds the current connection to active camera.
+	let camerarequest = null;
+
+	if(camerarequests[id]!=null){
+		socket.emit("error", `Error: Camera with id ${id} already in use.`, id);
+		return;
+	}
+	console.log("Client has been given a valid id.");
+	const consumer = new MjpegConsumer();
 
 	const Camera = CamData[id];
 
 	if (Camera == undefined) {
-		socket.emit("error", `Error: Camera with id ${id} can not be found.`);
+		socket.emit("error", `Error: Camera with id ${id} can not be found.`, id);
 		return;
 	}
 
 	//? http://mjpeg.sanford.io/count.mjpeg
-	camerarequest = request("http://mjpeg.sanford.io/count.mjpeg")
-		.on("error", (e) => {
-			console.log("unable to connect to camera, write error html.");
+	camerarequest = request(Camera.url)
+		.on("error", () => {
+			console.log("unable to connect to camera.");
 			socket.emit(
 				"error",
-				"Error: Camera is not working properly, connection failed."
+				"Error: Camera is not working properly, connection failed.",
+				id
 			);
-			camerarequest.end();
+			// Return null to object instance so if can be reset manually (important).
+			return null;
 		})
 		.on("response", () => {
-			console.log("Request is possible, writting headers.");
-			socket.emit("response");
+			console.log("Request is possible, writting response headers.");
+			socket.emit("response", id);
 		})
 		.pipe(consumer)
 		.on("data", (content) => {
-			socket.emit("frame", content);
+			socket.emit("frame", content, id);
 		});
+
+	camerarequests[id] = camerarequest;
 });
 
-socket.on("terminate",()=>{
-	if(camerarequest){
-		camerarequest.end();
+socket.on("terminate", (id) => {
+	if (camerarequests.at(id)) {
+		camerarequests.at(id).end();
+		camerarequests[id] = null;
 		console.log("camera terminated.");
 	}
 });
-
